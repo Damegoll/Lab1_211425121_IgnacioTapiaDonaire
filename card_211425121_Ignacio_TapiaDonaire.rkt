70b73d47ff50b4ca9c2404e6a69cb981a0f50945
Ignacio Tapia <ignacio.tapia.d@usach.cl>
2025-04-03 16:50:54 -0300

    Ultimos constructores

---
+++ card_211425121_Ignacio_TapiaDonaire.rkt
@@ -0,0 +1,9 @@
+#lang racket
+
+; Descripción: Funcion constructor para las cartas del juego CAPITALIA
+; DOM: id (int) tipo (string) descripcion (string) accion (funcion)
+; REC: card (lista)
+; Tipo recursión: No aplica
+
+(define (card id tipo descripcion accion)
+  (list id tipo descripcion accion))
