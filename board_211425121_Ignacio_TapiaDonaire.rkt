8a93eb429291a55334712da425ea5809926d73b8
Ignacio Tapia <ignacio.tapia.d@usach.cl>
2025-04-08 16:54:44 -0400

    se agrega TDA modificador tablero-agregar-propiedad/board-add-property

--- board_211425121_Ignacio_TapiaDonaire.rkt
+++ board_211425121_Ignacio_TapiaDonaire.rkt
@@ -1,9 +1,30 @@
 #lang racket
+(require "property_211425121_Ignacio_TapiaDonaire.rkt")
+(provide board-add-property)
+(provide board)
+
+
+
+
+
 
 ; Descripción: Constructor para guardar la información de las casillas en CAPITALIA
 ; DOM: property (lista) card-luck (lista) card-community (lista) special-space (lista)
 ; REC: board (lista)
 ; Tipo recursion: no aplica
 
-(define (board property card-luck card-community special-space)
-  (list property card-luck card-community special-space))
+(define (board add-property card-luck card-community special-space)
+  (list add-property card-luck card-community special-space))
+
+; -----------------------------------------------------------------
+
+;board-add-property
+
+;Descripción: Modificador para agregar propiedades al tablero
+;DOM: board-place (make-board) property-pos (lista de make-property pos)
+;REC: make-board (lista)
+;Tipo recursion: No aplica
+
+
+(define (board-add-property board-place new-property pos)
+  (list board-place (cons new-property (cons pos null))))
